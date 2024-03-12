e88fe10752f3c27b13c477f6c05aa96ce3a154fd
Artem Hordiyevskyy <artem2500@gmail.com>
2024-03-12 12:27:25 +0200

    add fakeStorageAPI

---
+++ src/lib/FakeStorageAPI.cpp
@@ -0,0 +1,27 @@
+// FakeStorageAPI.cpp
+#include "FakeStorageAPI.h"
+#include "Timeline.h"
+#include "Task.h"
+
+Timeline FakeStorageAPI::getExampleTimeline()
+{
+    Timeline timeline;
+    // initialize taskID
+    Task::setLastID(1);
+
+    // Example tasks
+    Task task1("Implement Task Class", true);
+    Task task2("Implement Timeline Class", true);
+    Task task3("Implement CLI Interface", true);
+    Task task4("Implement Reports", true);
+    Task task5("Get an excellent grade", true);
+
+    // Add tasks to timeline
+    timeline.addTask(task1);
+    timeline.addTask(task2);
+    timeline.addTask(task3);
+    timeline.addTask(task4);
+    timeline.addTask(task5);
+
+    return timeline;
+}
---
+++ src/lib/FakeStorageAPI.h
@@ -0,0 +1,14 @@
+// FakeStorageAPI.h
+#ifndef FAKE_STORAGE_API_H
+#define FAKE_STORAGE_API_H
+
+#include "Task.h"
+#include "Timeline.h"
+
+class FakeStorageAPI
+{
+public:
+    static Timeline getExampleTimeline();
+};
+
+#endif // FAKE_STORAGE_API_H
